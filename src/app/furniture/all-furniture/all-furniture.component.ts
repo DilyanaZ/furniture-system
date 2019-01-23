8ab7c5b714e0ecda8b2631e8b895984583a135cb
DilyanaZ <dililanataleva@gmail.com>
2019-01-23 15:38:05 +0200

    show  list of all created furniture

--- src/app/furniture/all-furniture/all-furniture.component.ts
+++ src/app/furniture/all-furniture/all-furniture.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FurnitureService } from '../furniture.service';
+import { FurnitureModel } from '../models/furniture.model';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-all-furniture',
@@ -6,10 +10,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-furniture.component.css']
 })
 export class AllFurnitureComponent implements OnInit {
+  furnitures: Observable <FurnitureModel[]>;
 
-  constructor() { }
+  constructor(private furnitureServise : FurnitureService) { }
 
+  //kato vzimame vs.items
   ngOnInit() {
+    this.furnitures = this.furnitureServise.getAllFurniture();
   }
 
 }
